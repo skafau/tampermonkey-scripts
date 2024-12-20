a94c661807c587839d9a03148bd4198bd8446469
Michael <michael@halumi.at>
2024-12-20 08:16:17 +0100

    Adapt nextcloud news script to latest news app

--- src/nextcloud-news-youtube-video-duration.user.js
+++ src/nextcloud-news-youtube-video-duration.user.js
@@ -34,36 +34,33 @@ GM_addStyle(`
 `);
 
 const observer = new MutationObserver(async (mutations, ob) => {
-  const titleLinkNodes = document.querySelectorAll('#app-content .utils .title h1 a');
-  if (!titleLinkNodes.length) return;
+  const feedItemRows = document.querySelectorAll('#news-app .feed-item-row');
+  if (!feedItemRows.length) return;
 
   // Random magic number. W/o this, the script doesn't work...
   await sleep(500);
 
-  for (let i = 0; i < titleLinkNodes.length; i++) {
-    const titleLinkNode = titleLinkNodes[i];
+  for (let i = 0; i < feedItemRows.length; i++) {
+    const feedItemRow = feedItemRows[i];
 
-    const h1NodeAlreadyHasLengthDiv = !!titleLinkNode.querySelector(`.${YT_VIDEO_LENGTH_DIV_CLASS}`);
-    if (h1NodeAlreadyHasLengthDiv) return;
+    const titleContainer = feedItemRow.querySelector('.title-container');
+    if (titleContainer.querySelector(`.${YT_VIDEO_LENGTH_DIV_CLASS}`)) continue;
 
-    const utilDiv = titleLinkNode.closest('div.utils');
-    const links = utilDiv.querySelectorAll('a');
-    const linkWithYouTubeLink = Array.from(links).find(link => link.href.includes('www.youtube.com'));
-    const youTubeLink = linkWithYouTubeLink?.href;
-    if (!youTubeLink) return;
+    const links = feedItemRow.querySelectorAll('.link-container a');
+    const youTubeLink = Array.from(links).find(link => link.href.includes('www.youtube.com'));
+    if (!youTubeLink) continue;
 
-    const { duration: durationIso8601, channelTitle } = await getYouTubeVideoInfo(youTubeLink);
+    const { duration: durationIso8601, channelTitle } = await getYouTubeVideoInfo(youTubeLink.href);
     const durationInSeconds = parseIso8601DurationToSeconds(durationIso8601);
     const durationInMinutes = durationInSeconds / 60;
     const durationInMinutesFormatted = `${Math.round(durationInMinutes)} minutes`;
 
-    const lengthDiv2 = titleLinkNode?.querySelector(`.${YT_VIDEO_LENGTH_DIV_CLASS}`);
-    if (lengthDiv2) return;
+    if (titleContainer.querySelector(`.${YT_VIDEO_LENGTH_DIV_CLASS}`)) continue;
 
     const newLengthDiv = document.createElement('div');
     newLengthDiv.className = YT_VIDEO_LENGTH_DIV_CLASS;
     newLengthDiv.textContent = `${durationInMinutesFormatted} - ${channelTitle}`;
-    titleLinkNode.append(newLengthDiv);
+    titleContainer.append(newLengthDiv);
   }
 });
 observer.observe(document.body, { childList: true, subtree: true });
@@ -109,6 +106,7 @@ async function getYouTubeVideoInfo(videoUrl) {
 
   const response = await fetch(apiUrl);
   const data = await response.json();
+  if (data.error) throw new Error(`Error fetching video info: ${data.error.message}`);
   if (data.items.length <= 0) throw new Error(`Video not found for URL ${videoUrl}`);
 
   const duration = data.items[0]?.contentDetails.duration;
